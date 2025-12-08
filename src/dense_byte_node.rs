b35bde19410975fb04f34d22d697b2686cdeb6fb
Luke Peterson <luke@infoplatter.com>
2025-12-08 00:14:20 -0700

    Fixing bug in ByteNode meet logic where a cofree's contents might encounter a dangling path, and the contents should anhilate, but the dangling path should remain

--- src/dense_byte_node.rs
+++ src/dense_byte_node.rs
@@ -1729,10 +1729,15 @@ impl<V: Clone + Send + Sync + Lattice, A: Allocator, Cf: CoFree<V=V, A=A>, Other
         rec_status.merge(val_status, true, true)
     }
     fn pmeet(&self, other: &OtherCf) -> AlgebraicResult<Self> {
-        //If two dangling paths meet, it's an identity result
-        if !self.has_rec() && !self.has_val() && !other.has_rec() && !other.has_val() {
-            return AlgebraicResult::Identity(SELF_IDENT | COUNTER_IDENT)
+        //If one or the other cofree is dangling, it's an identity result for the dangling cofree
+        let mut identity_flag = 0;
+        if !self.has_rec() && !self.has_val() {identity_flag = SELF_IDENT;}
+        if !other.has_rec() && !other.has_val() {identity_flag |= COUNTER_IDENT;}
+        if identity_flag > 0 {
+            return AlgebraicResult::Identity(identity_flag)
         }
+
+        //Otherwise actually work with what the cofrees contain
         let rec = self.rec().pmeet(&other.rec());
         let val = self.val().pmeet(&other.val());
         self.combine_algebraic_results(other, rec, val)
